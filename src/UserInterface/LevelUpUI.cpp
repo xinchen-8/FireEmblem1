f6f9395c10c00c14b7a306b2227a5c040f2ebdff
Rita <RitaChou10@gmail.com>
2025-06-11 17:29:39 +0800

    LevelUpUI can show the change of data

--- src/UserInterface/LevelUpUI.cpp
+++ src/UserInterface/LevelUpUI.cpp
@@ -1,10 +1,10 @@
 #include "UserInterface/LevelUpUI.hpp"
 
 LevelUpUI::LevelUpUI(std::vector<std::shared_ptr<Tile>>& tiles) : UserInterface(tiles) {
-    setUISize({5, 5});
+    setUISize({5, 7.5});
     setRelativePos({
-        -floor(PTSD_Config::WINDOW_WIDTH / 2) + 9 * TILE_SIZE,
-        -floor(PTSD_Config::WINDOW_HEIGHT / 2) + 5 * TILE_SIZE
+        -floor(PTSD_Config::WINDOW_WIDTH / 2) + 8.5 * TILE_SIZE,
+        -floor(PTSD_Config::WINDOW_HEIGHT / 2) + 2 * TILE_SIZE
     });
     SetZIndex(20);
 }
@@ -35,12 +35,15 @@ void LevelUpUI::update() {
 
     std::string content = 
         character->getName() + " Level Up!\n" +
-        std::to_string(character->getLevel()) + "  Exp " + std::to_string(character->getExp()) + "\n" + "HP  " +
-        std::to_string(character->getCurHP()) + " / " + std::to_string(character->getHpLimit()) + "\n" + "Str " +
-        std::to_string(character->getStr()) + "  Lck " + std::to_string(character->getLck()) + "\n" + "Skl " +
-        std::to_string(character->getSkl()) + "  Def " + std::to_string(character->getDef()) + "\n" + "Wlv " +
-        std::to_string(character->getWlv()) + "  Res " + std::to_string(character->getRes()) + "\n" + "Spd " +
-        std::to_string(character->getSpd()) + "  Mov " + std::to_string(character->getMov());
+        "Lv " + std::to_string(character->getLevel()) + "  Exp " + std::to_string(character->getExp()) + "\n" +
+        "HP " + std::to_string(character->getOldHpLimit()) + " -> " + std::to_string(character->getHpLimit()) + "\n" +
+        "Str " + std::to_string(character->getOldStr()) + " -> " + std::to_string(character->getStr()) + "\n" +
+        "Lck " + std::to_string(character->getOldLck()) + " -> " + std::to_string(character->getLck()) + "\n" +
+        "Skl " + std::to_string(character->getOldSkl()) + " -> " + std::to_string(character->getSkl()) + "\n" +
+        "Def " + std::to_string(character->getOldDef()) + " -> " + std::to_string(character->getDef()) + "\n" +
+        "Wlv " + std::to_string(character->getOldWlv()) + " -> " + std::to_string(character->getWlv()) + "\n" +
+        "Res " + std::to_string(character->getOldRes()) + " -> " + std::to_string(character->getRes()) + "\n" +
+        "Spd " + std::to_string(character->getOldSpd()) + " -> " + std::to_string(character->getSpd());
     setString(content);
 }
 
